7dfff64ef4c011e70e49129c2dd153f9c1ea9932
Ben Pilgrim <ben@pilgrim.me.uk>
2020-05-24 17:39:24 +0100

    Add Windows Terminal settings.json and AppData update script

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -5,9 +5,14 @@ cd "$(dirname "${BASH_SOURCE}")"
 
 rm ~/.bashrc
 
-for f in $(find ./ -maxdepth 1 -type d | grep -Ev '.git$|bin' | cut -d '/' -f 2 )
+for f in $(find ./ -maxdepth 1 -type d | grep -Ev '.git$|bin|appdata' | cut -d '/' -f 2 )
 do
     stow -R $f -t ~/
 done
+
+if [ -x "$(command -v cmd.exe)" ]; then
+	cp -a ./appdata/local/* $(wslpath -u $(powershell.exe '$env:LocalAppData') | sed 's/\r$//')
+	#cp -a ./appdata/roaming/* $(wslpath -u $(powershell.exe '$env:AppData') | sed 's/\r$//')
+fi
 
 popd > /dev/null
